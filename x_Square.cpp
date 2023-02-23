61667598f48b84d846efb3d614ab600c3e725fec
pod2c <52070996+pod2c@users.noreply.github.com>
2023-02-23 23:20:12 +0800

    Add files via upload

---
+++ x_Square.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0;
+        int r = x;
+        int mid;
+        int ans = -1;
+
+        while (l<r){
+            mid = l+(r-l)/2;
+            if (mid*mid<=x){
+                ans = mid;
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+};
